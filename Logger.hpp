abf773c1be99fdb143b794d9857a3f115b7655b4
Nick Fetcko <nick.fetcko@gmail.com>
2025-07-26 14:32:03 -0500

    Initial Commit

---
+++ Logger.hpp
@@ -0,0 +1,221 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string_view>
+#include <thread>
+
+#ifdef WIN32
+	#ifndef WIN32_LEAN_AND_MEAN
+		#define WIN32_LEAN_AND_MEAN
+	#endif
+
+	#include <windows.h>
+
+	// These Windows macros conflict
+	// with STL algorithms
+	#undef min
+	#undef max
+#endif
+
+namespace Fetcko {
+class LoggableClass;
+class Logger {
+public:
+	enum class Level {
+		Info,
+		Debug,
+		Warning,
+		Error
+	};
+
+	static inline Level logLevel = Level::Info;
+
+	void SetObject(LoggableClass *object) { this->object = object; }
+	void SetLogLevel(Level logLevel) { this->logLevel = logLevel; }
+
+	template <typename T>
+	void Log(T t) {
+		std::cout << t << std::endl;
+
+#ifdef WIN32
+		SetConsoleTextAttribute(
+			out,
+			static_cast<WORD>(WindowsConsoleColors::Default)
+		);
+#endif
+	}
+
+	template<typename T, typename... Args>
+	void Log(Level level, T t, Args... args) {
+		if (level >= logLevel) {
+			Log(level, t);
+			Log(args...);
+		}
+	}
+
+	template<typename T, typename... Args>
+	void Log(Level level, T t) {
+		if (level >= logLevel) {
+#ifdef WIN32
+			SetConsoleTextAttribute(
+				out,
+				static_cast<WORD>(Colors.at(level))
+			);
+#endif
+
+			const auto time = std::chrono::system_clock::to_time_t(
+				std::chrono::system_clock::now()
+			);
+
+			const auto &name = object->GetName();
+
+			std::cout
+				<< "["
+				<< Labels.at(level)
+				<< "] ("
+				<< std::put_time(std::localtime(&time), "%d%b%Y %H:%M:%S")
+				<< ") "
+				<< typeid(*object).name();
+
+			if (name.size()) {
+				std::cout
+					<< " ("
+					<< name
+					<< ")";
+			}
+
+			std::cout
+				<< " [0x"
+				<< std::hex
+				<< object
+				<< std::dec
+				<< "]: "
+				<< t;
+		}
+	}
+
+	template<typename T, typename... Args>
+	void LogInfo(T t, Args... args) {
+		Log(Level::Info, t, args...);
+	}
+
+	template<typename T, typename... Args>
+	void LogDebug(T t, Args... args) {
+		Log(Level::Debug, t, args...);
+	}
+
+	template<typename T, typename... Args>
+	void LogWarning(T t, Args... args) {
+		Log(Level::Warning, t, args...);
+	}
+
+	template<typename T, typename... Args>
+	void LogError(T t, Args... args) {
+		Log(Level::Error, t, args...);
+	}
+
+private:
+	template<typename T, typename... Args>
+	void Log(T t, Args... args) {
+		std::cout << t;
+		Log(args...);
+	}
+
+#ifdef WIN32
+	enum class WindowsConsoleColors {
+		Black,
+		DarkBlue,
+		DarkGreen,
+		DarkCyan,
+		DarkRed,
+		DarkMagenta,
+		DarkYellow,
+		Default,
+		Grey,
+		Blue,
+		Green,
+		Cyan,
+		Red,
+		Magenta,
+		Yellow,
+		White
+	};
+
+	static const inline std::map<Level, WindowsConsoleColors> Colors = {
+		{ Level::Info, WindowsConsoleColors::DarkCyan },
+		{ Level::Debug, WindowsConsoleColors::DarkGreen },
+		{ Level::Warning, WindowsConsoleColors::DarkYellow },
+		{ Level::Error, WindowsConsoleColors::DarkRed }
+	};
+
+	static inline const HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+#endif
+
+	static const inline std::map<Level, std::string_view> Labels{
+		{ Level::Info, "Info" },
+		{ Level::Debug, "Debug" },
+		{ Level::Warning, "Warning" },
+		{ Level::Error, "Error" }
+	};
+
+	LoggableClass *object = nullptr;
+};
+
+class LoggableClass {
+public:
+	LoggableClass() {
+		logger.SetObject(this);
+	}
+
+	LoggableClass(std::string &&name) :
+		LoggableClass() {
+		this->name = std::move(name);
+	}
+
+	// Make sure destructor is virtual
+	virtual ~LoggableClass() = default;
+
+	template<typename T, typename... Args>
+	void Log(Logger::Level level, T t, Args... args) {
+		logger.Log(level, t, args...);
+	}
+
+	template<typename T, typename... Args>
+	void LogInfo(T t, Args... args) {
+		logger.LogInfo(t, args...);
+	}
+
+	template<typename T, typename... Args>
+	void LogDebug(T t, Args... args) {
+		logger.LogDebug(t, args...);
+	}
+
+	template<typename T, typename... Args>
+	void LogWarning(T t, Args... args) {
+		logger.LogWarning(t, args...);
+	}
+
+	template<typename T, typename... Args>
+	void LogError(T t, Args... args) {
+		logger.LogError(t, args...);
+	}
+
+	virtual const std::string &GetName() const { return name; }
+
+protected:
+	Logger logger;
+
+	std::string name;
+};
+
+class LoggableThread : public std::thread, public LoggableClass {
+public:
+	LoggableThread(std::string &&name, std::function<void()> &&f) : std::thread(std::move(f)) {
+		this->name = std::move(name);
+	}
+	virtual ~LoggableThread() = default;
+};
+}
