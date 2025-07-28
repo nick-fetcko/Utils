4122f61ce872bc83fc7d0dc9a75dfe940711e63e
Nick Fetcko <nick.fetcko@gmail.com>
2025-07-28 15:44:38 -0500

    Re-enabling logging in GetStringFromFile()

--- Utils.cpp
+++ Utils.cpp
@@ -4,18 +4,16 @@
 #include <string>
 #include <sstream>
 
+#include "Logger.hpp"
+
 namespace Fetcko {
 std::string Utils::GetStringFromFile(const std::filesystem::path & path) {
 	std::ifstream inFile(path, std::ios_base::in | std::ios_base::binary);
 
 	if (!inFile) {
-		// FIXME: While CConsole allows for console _input_,
-		//        we really need to harmonize it with a
-		//        general-purpose logger.
-		/*
 		LoggableClass errorLog(typeid(Utils).name());
 		errorLog.LogError("File ", path.string(), " not found");
-		*/
+		
 		return "";
 	}
 
